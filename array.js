3a04c53e2f491ce15dd538596cac7c256d4c66ad
alishba-cmd <alishbawaqar7786@gmail.com>
2025-02-14 23:04:47 +0500

    Initial commit

---
+++ array.js
@@ -0,0 +1,6 @@
+const employees = [
+    { id: 1, name: "Alishba", age: 22},
+    { id: 2, name: "amna", age: 24}
+  ];
+  const employeess = employees.map(employees => employees.name);
+  console.log(employeess);
